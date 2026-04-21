c414d0a6bf92f6ae916909dca9ed92930f8f9d2b
PEREIRA Arthur <arthur.pereira@externe.maif.fr>
2026-04-21 11:24:00 +0200

    1

---
+++ src/main/java/com/brief2/api/jpalibrary/entity/Book.java
@@ -0,0 +1,58 @@
+package com.brief2.api.jpalibrary.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    private String description;
+
+    private Boolean available = true;
+
+    // Constructeurs
+    public Book() {
+    }
+
+    // Getters et Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+}
---
+++ src/main/java/com/brief2/api/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.brief2.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.brief2.api.jpalibrary.entity.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> { }
